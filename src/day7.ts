c305be48d30896a47759513843dc7e6482b99d96
Matti Launiemi <launiemi.matti@gmail.com>
2023-01-03 23:25:13 +0200

    day7 wip

--- src/day7.ts
+++ src/day7.ts
@@ -2,18 +2,53 @@
 import { readFile } from './readFile.js';
 
 type File = { type: string, path: string, size: number };
-
-const fileSystem: File = [];
+const fileSystem: File[] = [{ type: 'dir', path: ' ', size: 0 }];
+let position: string[] = [' '];
 
 const scanFiles = async (input: string) => {
   const commands: string[] = input.split(/\$/).map(s => s.trim());
-  // TODO
-
-  console.table(commands)
+  commands.filter(s => s).forEach(command => {
+    // Commands: cd, ls
+    if (command.match(/^cd/g)) {
+      const path = command.slice(3);
+      if (path === "/") position = [' '];
+      else if (path === "..") position.pop();
+      else if (path.match(/[a-zA-Z\d]./g)) position.push(path);
+      else throw new Error(`unknown path: "${path}"`);
+    }
+    else if (command.match(/^ls/g)) {
+      const lines: string[] = command.split(/[\r]?\n/);
+      lines.shift();
+      const files: File[] = lines.map(s => {
+        const file = s.split(' ');
+        if (file[0] === 'dir') {
+          return { type: 'dir', path: [...position, file[1]].join('/'), size: 0 };
+        }
+        return { type: 'file', path: [...position, file[1]].join('/'), size: Number(file[0]) };
+      });
+      // add files to filesystem
+      files.forEach(file => {
+        fileSystem.push(file);
+        if (file.type === 'file') {
+          // Add size to dir and subdirs
+          const filePath = file.path.split('/');
+          while (filePath.length > 1) {
+            filePath.pop();
+            let fileIndex = fileSystem.findIndex(f => f.type === 'dir' && f.path === filePath.join('/'));
+            if (fileIndex >= 0) fileSystem[fileIndex].size += file.size;
+            else console.log('fileIndex', fileIndex, filePath.join('/'));
+          }
+        }
+      });
+    }
+    else throw new Error(`unknown command: "${command}"`);
+  });
 }
 
 const partOne = async (): Promise<string> => {
-  return ``;
+  const fsSearch: File[] = fileSystem.filter(file => file.type==='dir' && file.size <= 100000);
+  console.table(fsSearch);
+  return `${fsSearch.reduce((accumulator, current: File) => accumulator + current.size, 0)}`;
 };
 
 const partTwo = async (): Promise<string> => {
@@ -25,6 +60,7 @@ export const main = async () => {
   try {
     const input: string = await readFile('./public/day7_input.txt');
     await scanFiles(input);
+    console.table(fileSystem);
     /* Part one */
     const message1 = await partOne();
     console.log(`Part One message ${message1}`);
