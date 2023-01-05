4d294aa986cffd091c8ca056ef5b63c6a761f625
Matti Launiemi <matti.launiemi@teliacygate.fi>
2023-01-05 15:05:06 +0200

    day10 done

--- src/day10.ts
+++ src/day10.ts
@@ -1,34 +1,88 @@
 import { readFile } from './readFile.js';
 
-const parseInput = async (input: string) => {
-  const data: number[][] = input.split(/\r?\n/)
-    .map(line => line.split('')
-      .map(s => Number(s))
-    )
+type Instruction = { command: string, value: number };
+
+class Computer {
+  public cycle: number = 0;
+  public registerX: number = 1;
+  public readCycles: number[] = [20, 60, 100, 140, 180, 220];
+  public signalStrengthReads: number[] = [];
+  public CrtIndex: number = 0;
+  public CRTs: string[] = [''];
+  public sprite: string = '';
+
+  public tick() {
+    this.cycle += 1;
+
+    // Read signal
+    if (this.readCycles.includes(this.cycle)) {
+      this.signalStrengthReads.push(this.getsignalStrength());
+    }
+    // Draw CRT
+    this.CRTs[this.CRTs.length - 1] += this.inSprite() ? '#' : ' ';
+
+    // CRT new row
+    if (this.CrtIndex > 0 && this.CrtIndex % 39 === 0) {
+      this.CRTs.push('');
+      this.CrtIndex = 0;
+    } else {
+      this.CrtIndex += 1;
+    }
+  }
+
+  public getsignalStrength(): number {
+    return this.cycle * this.registerX;
+  }
+
+  public addx(value: number) {
+    this.registerX += value;
+  }
+
+  public inSprite(): boolean {
+    const value = this.CrtIndex >= this.registerX - 1 && this.CrtIndex <= this.registerX + 1;
+    return value;
+  }
+}
+
+const parseInput = async (filePath: string): Promise<Instruction[]> => {
+  const input: string = await readFile(filePath);
+  const data = input.split(/\r?\n/)
+    .map(line => {
+      const [command, value] = line.split(' ');
+      return { command, value: Number(value) };
+    });
   return data;
 };
 
-const partOne = async (): Promise<string> => {
-  return `Todo`;
-};
+const partOne = async (instructions: Instruction[]) => {
+  const tupe = new Computer();
 
-const partTwo = async (): Promise<string> => {
-  return `Todo`;
+  for (const instruction of instructions) {
+    switch (instruction.command) {
+      case 'noop':
+        tupe.tick();
+        break;
+      case 'addx':
+        tupe.tick();
+        tupe.tick();
+        tupe.addx(instruction.value);
+        break;
+      default: throw new Error(`unknown command ${instruction.command}`);
+    }
+  }
+
+  console.log(`Part One: ${tupe.signalStrengthReads} sum ${tupe.signalStrengthReads.reduce((sum, signal) => sum + signal, 0)}`);
+  console.log(`Part two: `);
+  console.table(tupe.CRTs);
 };
 
 export const main = async () => {
   try {
     /* Parse input */
-    const input: string = await readFile('./public/day0_input.txt');
-    await parseInput(input);
-    
-    /* Part one */
-    const result1 = await partOne();
-    console.log(`Part One: ${result1}`);
-    
-    /* Part two */
-    const result2 = await partTwo();
-    console.log(`Part Two: ${result2}`);
+    const instructions = await parseInput('./public/input_day10.txt');
+
+    /* Part one and two*/
+    await partOne(instructions);
 
   } catch (e) {
     console.log(e);
