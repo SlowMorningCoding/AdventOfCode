ab8e4b69ed677dfb6053047027dc388c3bde0736
Matti Launiemi <launiemi.matti@gmail.com>
2023-01-10 22:03:48 +0200

    day13 wip

--- src/day13.ts
+++ src/day13.ts
@@ -15,7 +15,7 @@ async function main() {
     await partOne(data);
 
     /* Part two */
-    await partTwo();
+    await partTwo(data);
 
   } catch (e) {
     console.log(e);
@@ -24,7 +24,7 @@ async function main() {
 };
 
 async function parseInput(): Promise<string[][]> {
-  const input: string = await readFile('./public/input_day13_test.txt');
+  const input: string = await readFile('./public/input_day13.txt');
   const blocks: string[] = input.split(/(?:\r?\n){2}/);
 
   const data: string[][] | [][][] = blocks.map(b => {
@@ -38,39 +38,74 @@ async function parseInput(): Promise<string[][]> {
   return data;
 };
 
-async function partOne(data: string[][] | [][][]) {
-  for (const pair of data) {
-    if (typeof pair[0] === 'number' && typeof pair[1] === 'number') {
-      
-
+async function partOne(data: []) {
+  let rightOrder = 0;
+  data.forEach((pair, index) => {
+    //console.log(`Pair ${index+1}`);
+    
+    let result = comparePair(pair[0], pair[1]);
+    if (result < 0) {
+      rightOrder += index + 1;
     }
-  }
+    //console.log(`result ${result}\n`);
+  });
 
+  console.log(rightOrder);
+};
+
+function comparePair(a: any, b: any): number {
+  //console.log(`${typeof a}, ${typeof b}`, a, b);
   /*
   If both are integers, the lower integer should come first. 
   If the left integer is lower than the right integer, the inputs are in the right order. 
   If the left integer is higher than the right integer, the inputs are not in the right order. 
   If the inputs are the same integer; continue checking the next part of the input.
-
+  */
+  if (typeof a === 'number' && typeof b === 'number') {
+    return (a - b);
+  }
+  /*
   If both values are lists, compare the first value of each list, then the second value, and so on. 
   If the left list runs out of items first, the inputs are in the right order. 
   If the right list runs out of items first, the inputs are not in the right order. 
   If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
-
+  */
+  else if (typeof a === 'object' && typeof b === 'object') {
+    let maxLength = a.length - b.length < 0 ? b.length : a.length;
+    for (let i = 0; i < maxLength; i++) {
+      const result: number = comparePair(a[i], b[i]);
+      if (result !== 0) return result;
+    }
+    return 0;
+  }
+  else if (typeof a === 'undefined') return -1;
+  else if (typeof b === 'undefined') return 1;
+  /*
   If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, 
   then retry the comparison. 
   */
-
-};
-
-function comparePair(a:number, b:number) {
-  if (a - b < 0) return -1;
-  if (a - b > 0) return 1;
-  return 0
+  else if (typeof a === 'object' && typeof b === 'number') {
+    return comparePair(a,[b]);
+  }
+  else if (typeof a === 'number' && typeof b === 'object') {
+    return comparePair([a],b);
+  }
+  throw new Error("unknown case!");
 }
 
-async function partTwo() {
+async function partTwo(data) {
+  const items = [];
+  data.forEach(pair => {
+    items.push(...pair);
+  });
+  items.sort(comparePair);
 
+  let sItems = items.map(a => JSON.stringify(a))
+  console.log(sItems);
+  console.log(sItems.indexOf('[[[2]]]'));
+  console.log(sItems.indexOf('[[[6]]]'));
+  
+  
 };
 
 await main();
